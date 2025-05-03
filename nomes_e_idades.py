0bbfb9aea6347238719c1f51fa999c76b0ad4d49
Maria <shark07856@sharkcoders.pt>
2025-05-03 10:28:35 +0100

    Add files via upload

--- nomes_e_idades.py
+++ nomes_e_idades.py
@@ -1,26 +1,58 @@
-print("\nManipulação de ficheiros\n")
-name = input("Nome do ficheiro \n>")
+from time import sleep
+
+print("""
+------------------------
+Manipulação de ficheiros
+------------------------
+""")
+name = input("Nome do ficheiro para abrir: \n>>>")
+print("""
+Aqui está o conteúdo do ficheiro aberto:
+""")
 with open(f"{name}.txt", 'r') as ficheiro:
     linhas = ficheiro.readlines()
     for linha in linhas:
         print(linha)
 
 while True:
-    nmi=input("\nQuer adicionar mais um nome e idade? \n1)Sim \n2)Não \n3)Procurar nome e idade\n \n>")
+    sleep(2)
+    nmi=input("""
+----------------MENU----------------
+Quer adicionar mais um nome e idade? 
+
+0)Sair
+1)Adicionar nome e idade
+2)Procurar nome e idade
+3)Ler o conteúdo do ficheiro
+>>>
+------------------------------------
+""")
     if nmi =="1":
-        conteudo = input("Digite o nome e a idade \n>")
+        conteudo = input("""
+------------------------------------------
+Digite o nome e a idade que quer adicionar 
+>>>""")
         with open(f"{name}.txt", 'a', encoding="utf-8") as ficheiro:
             ficheiro.write(conteudo + "\n")
         print("Feito!")
     elif nmi == "2":
-        print("Até a próxima.")
-        break
-    else:
-        palavra = input("Que palavra gostaria de procurar? \n>")
+        palavra = input("Que nome gostaria de procurar? \n>")
         with open(f"{name}.txt", "r", encoding='utf-8') as ficheiro:
             for linha in ficheiro:
                 if palavra in linha:
                     print("Aqui esta seu resultado:")
                     print(linha.rstrip())
+    elif nmi == "3":
+        print("""
+Aqui está o conteúdo do ficheiro aberto:
+        """)
+        with open(f"{name}.txt", 'r') as ficheiro:
+            linhas = ficheiro.readlines()
+            for linha in linhas:
+                print(linha)
+    else:
+        print("Até à próxima!")
+        break
+
 
 
